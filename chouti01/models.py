57cac6d1105b29e9888a67defdccab23ae42c8c7
zhangci <843248880@qq.com>
2016-12-24 10:34:13 +0800

    tiaozhuan dao  index.html bu xianshi yonghu .

--- chouti01/models.py
+++ chouti01/models.py
@@ -6,3 +6,5 @@ class UserInfo(models.Model):
     name = models.CharField(max_length=30,null=True)
     phone = models.CharField(max_length=30,null=True)
     pwd = models.CharField(max_length=30)
+    logindate = models.DateTimeField(auto_now=True)
+    registerdate = models.DateTimeField(auto_now_add=True)
--- chouti01/views.py
+++ chouti01/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 
 # Create your views here.
 
@@ -9,9 +9,8 @@ from django.forms.utils import ErrorDict
 from django.core.exceptions import ValidationError
 
 def index(request):
+    print('333')
     return render(request,'index.html')
-
-
 
 
 def mobile_validate(value):
@@ -33,22 +32,49 @@ class UserLoginForm(forms.Form):
 class UserRegisterForm(forms.Form):
     registermobile = forms.CharField(validators=[mobile_validate, ],
                             error_messages={'required': u'手机不能为空'},)
-    registerpwd = forms.CharField(min_length=6,max_length=15,error_messages={'required':'不能为空','min_length':'最小长度是6','max_length':'最大长度是15'})
+    registerpwd = forms.CharField(min_length=8,max_length=15,error_messages={'required':'不能为空','min_length':'最小长度是6','max_length':'最大长度是15'})
 
 def login(request):
     if request.method == 'POST':
         result = {'status':False,'message':''};
         login_choice = eval(json.dumps(request.POST))['login_choice']
         if login_choice == 'phoneLogin':
-
+            username = eval(json.dumps(request.POST))['mobile']
+            password = eval(json.dumps(request.POST))['loginpwd']
             obj = PhoneLoginForm(request.POST)
-            print(request.POST)
             ret = obj.is_valid()
         elif login_choice == 'userLogin':
+            username = eval(json.dumps(request.POST))['userid']
+            password = eval(json.dumps(request.POST))['loginpwd']
             obj = UserLoginForm(request.POST)
             ret = obj.is_valid()
         if ret:
-            result['status'] = True
+            if login_choice == 'phoneLogin':
+                mobile_num = models.UserInfo.objects.filter(phone=username).count()
+                if mobile_num <= 0:
+                    result['message'] = {'loginErrorMessage': [{'phonelogin': 'noTheUser', 'message': '该手机尚未注册'}]}
+                else:
+                    pwd_sql = models.UserInfo.objects.filter(phone=username).values_list('pwd').first()[0]
+                    if password == pwd_sql:
+                        result['status'] = True
+                        # loginedUser = username
+                        # print('234')
+                        # return render(request,'index.html',{'loginedUser':loginedUser})
+                        # return redirect('/index/')
+                    else:
+                        result['message'] = {'loginErrorMessage': [{'phonelogin': 'noTheUser', 'message': '手机号或密码错误'}]}
+
+            elif login_choice == 'userLogin':
+                user_num = models.UserInfo.objects.filter(name=username).count()
+                if user_num <= 0:
+                    result['message'] = {'loginErrorMessage': [{'phonelogin': 'noTheUser', 'message': '该用户尚未注册'}]}
+                else:
+
+                    pwd_sql = models.UserInfo.objects.filter(user=username).values_list('pwd').first()[0]
+                    if password == pwd_sql:
+                        result['status'] = True
+                    else:
+                        result['message'] = {'loginErrorMessage': [{'phonelogin': 'noTheUser', 'message': '用户名或密码错误'}]}
         else:
             result['message'] = json.loads(obj.errors.as_json())
 
@@ -63,11 +89,17 @@ def register(request):
         result = {'status': False, 'message': ''};
         obj = UserRegisterForm(request.POST)
         ret = obj.is_valid()
-        if ret:
+        mobile_num = models.UserInfo.objects.filter(phone=registermobile).count()
+        if ret and mobile_num == 0:
+            print('1');
             result['status'] = True
             models.UserInfo.objects.create(phone=registermobile, pwd=registerpwd)
-            print(models.UserInfo.objects.filter(phone='17769040425').count())
+        elif ret and mobile_num != 0:
+            print('2');
+            result['status'] = False
+            result['message'] = {'thePhoneIsUsed': [{'register': 'phoneIsUsed', 'message': '该手机号已注册'}]}
         else:
+            print(json.loads(obj.errors.as_json()))
             result['message'] = json.loads(obj.errors.as_json())
 
         return HttpResponse(json.dumps(result))
